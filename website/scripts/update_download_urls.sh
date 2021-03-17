a78f082dde3a96708aadc64601ca49478cf336cf
Eric Stroczynski <ericstroczynski@gmail.com>
2021-03-17 13:12:53 -0700

    netlify.toml: redirects for latest version, have update script update this version on prerelease too
    Signed-off-by: Eric Stroczynski <ericstroczynski@gmail.com>

---
+++ website/scripts/update_download_urls.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+# This script updates the operator-sdk download link with the current release version.
+# This change should be committed in the prerelease commit.
+
+set -e
+
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
+
+VERSION="${1?"A Version is required"}"
+
+DOC_PATH="${DIR}/../content/en/docs/installation/_index.md"
+TARGET="(export OPERATOR_SDK_DL_URL=https://github.com/operator-framework/operator-sdk/releases/).+"
+
+sed -i -E 's@'"${TARGET}"'@\1'"${VERSION}"'@g' "$DOC_PATH"
+
+# Ensure the file was updated.
+if ! grep -q "OPERATOR_SDK_DL_URL=https://github\.com/operator-framework/operator-sdk/releases/${VERSION}" "$DOC_PATH"; then
+  echo "$0 failed to update ${DOC_PATH}"
+  exit 1
+fi
+
+CONFIG_PATH="${DIR}/../../netlify.toml"
+TARGET="(https://sdk\.operatorframework\.io/dl/)v[^/]+(.+)"
+
+sed -i -E 's@'"${TARGET}"'@\1'"${VERSION}"'\2@g' "$CONFIG_PATH"
+
+# Ensure the file was updated.
+if ! grep -q "https://sdk\.operatorframework\.io/dl/${VERSION}" "$CONFIG_PATH"; then
+  echo "$0 failed to update ${CONFIG_PATH}"
+  exit 1
+fi
